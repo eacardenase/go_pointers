43cbc9f02e93a80f0d5aee94ea78c52a9188c857
Edwin Cardenas <eacardenase@gmail.com>
2024-11-05 11:57:26 -0500

    feat: use pointers value for math operations

--- pointers.go
+++ pointers.go
@@ -3,15 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	age := 28
-	var agePointer *int // declaration
-	agePointer = &age   // instantiation
+	age := 32
+	agePointer := &age
+	// var agePointer *int // declaration
+	// agePointer = &age   // instantiation
 
 	fmt.Println("Age Address:", agePointer) // 0x1400009a02
 	fmt.Println("Age Value:", *agePointer)  // 28
 
-	// adultYears := getAdultYears(agePointer)
-	// fmt.Println(adultYears)
+	// adultYears := getAdultYears(&age)
+	adultYears := getAdultYears(agePointer)
+	fmt.Println(adultYears)
 }
 
 func getAdultYears(age *int) int {
